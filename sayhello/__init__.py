584a61ffa2e2272543bd3246128a16e845a269af
MingruiWang2017 <1287658016@qq.com>
2022-03-14 12:15:11 +0800

    Using Moment.js format timestamp

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap4
 from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
 
 app = Flask('sayhello')
 app.config.from_pyfile('settings.py')  # 从文件中导入配置
@@ -9,5 +10,6 @@ app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap4(app)
+moment = Moment(app)
 
 from sayhello import views, errors, commands
--- sayhello/models.py
+++ sayhello/models.py
@@ -7,5 +7,5 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     body = db.Column(db.String(200))
-    timestamp = db.Column(db.DateTime, default=datetime.now, index=True)
-    # 注意，这里设置的默认值是方法名称now，不是方法的调用now()
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+    # 注意，这里设置的默认值是方法名称utcnow，不是方法的调用utcnow()
