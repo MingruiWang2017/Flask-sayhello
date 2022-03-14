319dd88bfd2888bea660ca9738bd07d819213cbd
MingruiWang2017 <1287658016@qq.com>
2022-03-14 10:48:39 +0800

    rendering HTML with Bootstrap-Flask

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap4
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('sayhello')
@@ -7,5 +8,6 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap4(app)
 
 from sayhello import views, errors, commands
